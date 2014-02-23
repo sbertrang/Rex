367c874643bd01239511fd4bf07422c5731d29b5
Simon Bertrang <janus@fabams.nl>
2014-02-23 21:07:03 +0100

    initial version of start command

---
+++ lib/Rex/Virtualization/QEMU/start.pm
@@ -0,0 +1,49 @@
+#
+# (c) Simon Bertrang <janus@cpan.org>
+# 
+# vim: set ts=3 sw=3 tw=0:
+# vim: set expandtab:
+
+package Rex::Virtualization::QEMU::start;
+
+use strict;
+use warnings;
+
+use Rex::Logger;
+use Rex::Helper::Run;
+use Rex::Commands::File;
+use Rex::Commands;
+use Rex::Helper::Path;
+
+use Rex::Virtualization::QEMU::info;
+
+sub execute {
+   my ( $class, $arg1, %opt ) = @_;
+
+   unless ( $arg1 ) {
+      die("You have to define the vm name!");
+   }
+
+   my $dom = $arg1;
+   Rex::Logger::debug("starting domain: $dom");
+
+   my $vm = Rex::Virtualization::QEMU::info->execute( $dom );
+
+   unless ( $vm ) {
+      die("VM $dom not found.");
+   }
+
+   my $bin = "qemu-system-$vm->{system}";
+   my $args = $vm->{args};
+   my $home = resolv_path( "~/" );
+
+   s!~/!$home! for @$args;
+
+   my $output = i_run "$bin @$args";
+
+   if ( $? != 0 ) {
+      die("Error starting domain: $dom");
+   }
+}
+
+1;
