aa34b58ef8f8d82bcdd2aad287f15b9631c4a655
Simon Bertrang <janus@fabams.nl>
2014-02-23 18:03:35 +0100

    add initial shutdown command

---
+++ lib/Rex/Virtualization/QEMU/shutdown.pm
@@ -0,0 +1,46 @@
+#
+# (c) Simon Bertrang <janus@cpan.org>
+# 
+# vim: set ts=3 sw=3 tw=0:
+# vim: set expandtab:
+
+package Rex::Virtualization::QEMU::shutdown;
+
+use strict;
+use warnings;
+
+use Rex::Logger;
+use Rex::Helper::Run;
+
+use Rex::Virtualization::QEMU::list;
+
+sub execute {
+   my ( $class, $arg1, %opt ) = @_;
+
+   unless ( $arg1 ) {
+      die("You have to define the vm name!");
+   }
+
+   my $dom = $arg1;
+   Rex::Logger::debug("shutdown domain: $dom");
+
+   my $vms = Rex::Virtualization::QEMU::list->execute( "all" );
+
+   my ( $vm ) = grep $_->{name} eq $dom, @$vms;
+
+   unless( $vm ) {
+      die("VM $dom not found.");
+   }
+
+   unless ( $vm->{status} eq "running" ) {
+      die("VM $dom not running.");
+   }
+
+   i_run "echo shutdown | nc -U $vm->{dir}/qemu.monitor";
+   if ( $? != 0 ) {
+      die("Error shutdown vm $dom");
+   }
+
+}
+
+1;
