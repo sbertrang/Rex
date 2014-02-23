84ed7a0ef6054fb3fda638ebbd14ce8db8aadb52
Simon Bertrang <janus@fabams.nl>
2014-02-23 17:40:20 +0100

    add initial info command implementation

---
+++ lib/Rex/Virtualization/QEMU/info.pm
@@ -0,0 +1,75 @@
+#
+# (c) Simon Bertrang <janus@cpan.org>
+#
+# vim: set ts=3 sw=3 tw=0:
+# vim: set expandtab:
+
+package Rex::Virtualization::QEMU::info;
+
+use strict;
+use warnings;
+
+use Rex::Logger;
+use Rex::Helper::Run;
+use Rex::Commands::File;
+
+use Data::Dumper;
+
+use Rex::Virtualization::QEMU::list;
+
+sub execute {
+   my ($class, $vmname) = @_;
+
+   unless($vmname) {
+      die("You have to define the vm name!");
+   }
+
+   Rex::Logger::debug("Getting info of domain: $vmname");
+
+   my $vms = Rex::Virtualization::QEMU::list->execute( "all" );
+
+   my ( $vm ) = grep $_->{name} eq $vmname, @$vms;
+
+   unless ( $vm ) {
+      return;
+   }
+
+   my $cmd = cat "$vm->{dir}/qemu.cmd";
+
+   if ( $? != 0 ) {
+      die("Error reading QEMU command file");
+   }
+
+   my %ret = %$vm;
+
+   my ( $prog, @opts ) = split m!\n!, $cmd;
+
+   if ( $prog =~ m!\A\Qqemu-system-\E(\S+)! ) {
+      $ret{system} = $1;
+   }
+   else {
+      die("Error reading QEMU system");
+   }
+
+   my %opts;
+
+   for my $line ( @opts ) {
+      my ( $opt, $val ) = split m!\s+!, $line, 2;
+
+      unless ( exists $opts{ $opt } ) {
+         $opts{ $opt } = $val;
+      }
+      else {
+         unless ( ref $opts{ $opt } ) {
+            $opts{ $opt } = [ $opts{ $opt } ];
+         }
+         push( @{ $opts{ $opt } }, $val );
+      }
+   }
+
+   $ret{opts} = \%opts;
+
+   return \%ret;
+}
+
+1;
