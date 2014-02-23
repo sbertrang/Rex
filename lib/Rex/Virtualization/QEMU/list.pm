544b53b9047f793c3e203036f578d4f90f7e0bea
Simon Bertrang <janus@fabams.nl>
2014-02-23 16:47:52 +0100

    implement minimal qemu list command

---
+++ lib/Rex/Virtualization/QEMU/list.pm
@@ -0,0 +1,69 @@
+#
+# (c) Simon Bertrang <janus@cpan.org>
+# 
+# vim: set ts=3 sw=3 tw=0:
+# vim: set expandtab:
+
+package Rex::Virtualization::QEMU::list;
+
+use strict;
+use warnings;
+
+use Rex::Logger;
+use Rex::Commands::File;
+use Rex::Commands::Fs;
+use Rex::Commands::Process;
+
+sub execute {
+   my ( $class, $arg1, %opt ) = @_;
+   my $user = Rex::Config->get_user();
+   my $dir = "~/.qemu/vms";
+   my $cmdfile = "qemu.cmd";
+   my $pidfile = "qemu.pid";
+
+   # lookup directory names
+   my @vms = map m!/([^/]+)/\Q$cmdfile\E\z! ? $1 : (),
+      glob "$dir/*/$cmdfile";
+   my %vms;
+
+   # get qemu processes for the given user only
+   my %ps = map +( $_->{pid}, $_ ),
+      grep $_->{user} eq $user &&
+           $_->{command} =~ m/\b\Qqemu-system-\E/,
+      ps();
+
+   for my $vm ( @vms ) {
+      my $pid = cat "$dir/$vm/$pidfile";
+
+      if ( $pid && $pid =~ m/\A\s*([0-9]+)\b\s*/m ) {
+         $pid = $1;
+      }
+      else {
+         $pid = undef;
+      }
+
+      my $status = $pid && exists $ps{ $pid }
+                 ? "running"
+                 : "stopped";
+
+      $vms{$vm} = {
+         name   => $vm,
+         pid    => $pid,
+         status => $status,
+      };
+   }
+
+   if ( $arg1 eq "all" ) {
+      @vms = sort @vms;
+   } elsif ( $arg1 eq "running" ) {
+      @vms = sort grep $vms{$_}{status} eq "running", @vms;
+   } else {
+      return;
+   }
+
+   my @ret = @vms{ @vms };
+
+   return \@ret;
+}
+
+1;
